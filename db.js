5d864642a056a01024427ceca75a8d121697780a
Ashwin Kumar Singh <ashwinkumar2309@protonmail.com>
2023-03-11 00:19:49 +0530

    backend done,comments added

--- db.js
+++ db.js
@@ -1,7 +1,11 @@
+// Import the mongoose library for Node.js to connect to MongoDB database
 const mongoose = require("mongoose");
+
+// Declare the uri of the mongodb atlas cluster where the data is stored
 const mongoUri =
   "mongodb+srv://ash2309:saymyname@ashdev.t5njj.mongodb.net/test";
 
+// Connect to the mongodb database and specify properties of this connection
 mongoose
   .connect(mongoUri, {
     useNewUrlParser: true,
@@ -9,7 +13,7 @@ mongoose
     useCreateIndex: true,
     useFindAndModify: false,
   })
-  .then(() => console.log("Connected to database"))
-  .catch((err) => console.log("Error connecting to database", err));
+  .then(() => console.log("Connected to database")) // Log a message on successful connection
+  .catch((err) => console.log("Error connecting to database", err)); // Catch any errors in the connection
 
-module.exports = mongoose.connection;
+// Export the established connection object model by calling the function mongoose.connection from another file.
--- middlewares/authenticator.js
+++ middlewares/authenticator.js
@@ -1,19 +1,32 @@
+// Import the jwt module to verify token
 const jwt = require("jsonwebtoken");
 
+/*
+ * Authenticator middleware function that takes three parameters req, res and next
+ * If the token is valid, then the function call the next() callback function to move on to the next middleware function, if not, then send an appropriate error response to the client.
+ */
 function authenticator(req, res, next) {
+  // Get the user's token from the headers of the request
   const token = req.headers.authorization;
-  // we send authorization header with the token
+
+  // Verify the retrieved token using secretkey in case it matches the stored one
   jwt.verify(token, "secretkey", (err, decode) => {
     if (err) {
+      // Error case when the token is not valid or expired
       res.send({
         message: "Token is not valid, please login",
         status: 2,
       });
     }
     if (decode) {
+      // Token is valid case
+      // Adding the decoded User's id to the requester body to be used in further other requests
       req.body.user = decode.userId;
+
+      // Calling next to proceed to the next middleware function or main request handler
       next();
     } else {
+      // Any other errors
       res.send({
         message: "Token is not valid, please login",
         status: 2,
@@ -21,4 +34,6 @@ function authenticator(req, res, next) {
     }
   });
 }
+
+// Exporting authenticator function to be used and accessed by other modules using require()
 module.exports = authenticator;
